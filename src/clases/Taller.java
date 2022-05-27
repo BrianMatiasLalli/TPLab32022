26880258cda4f3870fffbe4edaaa9bfa6a69032a
BrianMatiasLalli <69776031+BrianMatiasLalli@users.noreply.github.com>
2022-05-27 14:37:05 -0300

    Update Taller.java
    agregamos comportamiento de fila a la clase taller

--- src/clases/Taller.java
+++ src/clases/Taller.java
@@ -10,4 +10,47 @@ public class Taller {
     public Taller() {
         this.serviciosTomados = new LinkedList<>();
     }
+    
+	
+	public void agregar(Servicio elemento) 
+	{
+		
+		serviciosTomados.addLast(elemento);
+		
+	}
+	
+	public Servicio remover() 
+	{
+		
+		return serviciosTomados.poll();
+	}
+	
+	public Servicio primerElemento() 
+	{
+		return serviciosTomados.peekFirst();
+	}
+	
+	public Servicio ultimoElemento() {
+		return serviciosTomados.peekLast();
+	}
+	
+	public String mostrar() 
+	{
+		StringBuilder sb= new StringBuilder();
+		LinkedList<Servicio> aux= new LinkedList<>();
+		
+		while(!serviciosTomados.isEmpty()) 
+		{
+			aux.add(serviciosTomados.pollLast());
+			sb.append(aux.peekLast() +"\n");
+		}
+		
+		while(!aux.isEmpty()) 
+		{
+			serviciosTomados.add(aux.pollLast());
+		}
+		
+		return sb.toString();
+	}
 }
+
